d85fab05364f4155394682cf318bd3043d1709e3
davecy <willido@protonmail.com>
2020-10-05 02:39:55 +0330

    add unit test for /postage GET endpoint

--- test/unit/post-office.unit.js
+++ test/unit/post-office.unit.js
@@ -50,4 +50,33 @@ describe('#post-office.js', () => {
       })
     })
   })
+
+  describe('#getPostageEndpoint', () => {
+    it('should return postage rate data', async function () {
+      // Mock network calls.
+      sandbox
+        .stub(Network.prototype, 'validateSLPInputs')
+        .resolves([])
+      sandbox
+        .stub(Network.prototype, 'fetchUTXOsForNumberOfStampsNeeded')
+        .resolves([])
+      sandbox
+        .stub(Network.prototype, 'broadcastTransaction')
+        .resolves({})
+
+      chai.request(server).get('/postage').end((err, res) => {
+        assert.equal(res.status, 200)
+
+        const result = JSON.parse(res.text)
+        assert.property(result, 'version')
+        assert.property(result, 'address')
+        assert.property(result, 'weight')
+        assert.property(result, 'stamps')
+        assert.isArray(result.stamps)
+
+        assert.isNull(err)
+      })
+    })
+  })
+
 })
